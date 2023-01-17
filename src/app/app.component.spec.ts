a6060da05ff7dda579637a8f19cb88cfee1ac504
Vitor Gabriel <gabrijac57@gmail.com>
2023-01-17 15:27:02 -0400

    refactor: finalização do projeto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'app-todo'`, () => {
+  it(`should have as title 'todo-list'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app-todo');
+    expect(app.title).toEqual('todo-list');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('app-todo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'todo-list app is running!'
+    );
   });
 });
--- src/app/modules/home/components/todo-button-delete-all/todo-button-delete-all.component.ts
+++ src/app/modules/home/components/todo-button-delete-all/todo-button-delete-all.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-todo-button-delete-all',
   templateUrl: './todo-button-delete-all.component.html',
-  styleUrls: ['./todo-button-delete-all.component.scss']
+  styleUrls: ['./todo-button-delete-all.component.css'],
 })
-export class TodoButtonDeleteAllComponent {
-
-}
+export class TodoButtonDeleteAllComponent {}
